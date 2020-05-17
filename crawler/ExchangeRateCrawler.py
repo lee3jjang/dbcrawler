b9e08ed310e84bbdc0ee70c2b7552f77bf0e8c11
이상진 <lee3jjang@dbins.co.kr>
2020-05-17 14:51:00 +0900

    환율 추가

--- crawler/ExchangeRateCrawler.py
+++ crawler/ExchangeRateCrawler.py
@@ -7,14 +7,14 @@ from .DBCrawler import DBCrawler
 """
     Description
     -----------
-    주가 데이터를 수집하는 크롤러
+    환율 데이터를 수집하는 크롤러
 """
 
-class StockPriceCrawler(DBCrawler):
+class ExchangeRateCrawler(DBCrawler):
     
     def __init__(self, conn):
         super().__init__(conn)
-        self.table_name = 'STOCK_PRICE'
+        self.table_name = 'EXCHANGE_RATE'
         self._create_table()
         
     def _create_table(self):
@@ -28,7 +28,7 @@ class StockPriceCrawler(DBCrawler):
             CREATE TABLE IF NOT EXISTS {table_name} (
                 BASE_DATE TEXT,
                 CODE TEXT,
-                PRICE NUMBER,
+                RATE NUMBER,
                 PRIMARY KEY (CODE, BASE_DATE)
             )
         """.format(table_name=self.table_name)
@@ -39,43 +39,43 @@ class StockPriceCrawler(DBCrawler):
         """
             Description
             -----------
-            수집할 주가 코드들 설정
+            수집할 환율 코드들 설정
             
             Input
             -----
-            code : 005830(DB손해보험), 005930(삼성전자), 105560(KB금융)
+            code : FX_USDKRW(원/달러), FX_JPYKRW(원/엔), FX_CNYKRW(원/위안)
             
             Example
             -------
             conn = sqlite3.connect('external_data.db')
-            spc = StockPriceCrawler(conn)
-            spc.set_code(['005830', '005930', '105560'])
+            erc = ExchangeRateCrawler(conn)
+            erc.set_code(['FX_USDKRW', 'FX_JPYKRW', 'FX_CNYKRW'])
         """
         
         self.codes = codes
         
     @staticmethod
-    def get_stock_price(code):
+    def get_exchange_rate(code):
         """
             Description
             -----------
-            주가 데이터 수집 프로그램(from 네이버)
+            환율 데이터 수집 프로그램(from 네이버)
 
             Input
             -----
-            code : 005830(DB손해보험), 005930(삼성전자), 105560(KB금융)
+            code : FX_USDKRW(원/달러), FX_JPYKRW(원/엔), FX_CNYKRW(원/위안)
 
             Output
             ------
-            일별 유가(종가)
+            일별 환율(매매기준율)
 
             Example
             -------
-            stock_price_db = StockPriceCrawler.get_stock_price('005830')
+            exchange_rate_jpykrw = ExchangeRateCrawler.get_exchange_rate('FX_JPYKRW')
 
         """
 
-        delay = 0.01
+        delay = 0.02
         page = 1
         result = []
         start_time = datetime.now()
@@ -83,7 +83,7 @@ class StockPriceCrawler(DBCrawler):
         # 수집
         print('[{}] 데이터 수집을 시작합니다. (code: {})'.format(start_time.strftime('%Y/%m/%d %H:%M:%S'), code))
         while(True):
-            url = 'https://finance.naver.com/item/sise_day.nhn?code={}&page={}'.format(code, page)
+            url = 'https://finance.naver.com/marketindex/exchangeDailyQuote.nhn?marketindexCd={}&page={}'.format(code, page)
             data = pd.read_html(url)[0].dropna()
             if page != 1:
                 try:
@@ -95,16 +95,16 @@ class StockPriceCrawler(DBCrawler):
             page += 1
             time.sleep(delay)
             
-        stock_price = pd.concat(result).reset_index(drop=True)
-        stock_price.columns = ['BASE_DATE', 'PRICE', '전일비', '시가', '고가', '저가', '거래량']
-        stock_price['BASE_DATE'] = stock_price['BASE_DATE'].apply(lambda x: datetime.strptime(x, '%Y.%m.%d').strftime('%Y-%m-%d'))
-        stock_price = stock_price[['BASE_DATE', 'PRICE']]
-        stock_price.insert(1, 'CODE', code)
-        
+        exchange_rate = pd.concat(result).reset_index(drop=True)
+        exchange_rate.columns = ['BASE_DATE', 'RATE', '전일대비', '사실 때', '파실 때', '보내실 때', '받으실 때', 'T/C 사실 때', '외화수표 파실 때']
+        exchange_rate['BASE_DATE'] = exchange_rate['BASE_DATE'].apply(lambda x: datetime.strptime(x, '%Y.%m.%d').strftime('%Y-%m-%d'))
+        exchange_rate = exchange_rate[['BASE_DATE', 'RATE']]
+        exchange_rate.insert(1, 'CODE', code)
+
         end_time = datetime.now()
-        print('[{}] 데이터 수집을 종료합니다. (code: {}, 수집시간: {}초, 데이터수: {:,}개)'.format(end_time.strftime('%Y/%m/%d %H:%M:%S'), code, (end_time-start_time).seconds, len(stock_price)))
+        print('[{}] 데이터 수집을 종료합니다. (code: {}, 수집시간: {}초, 데이터수: {:,}개)'.format(end_time.strftime('%Y/%m/%d %H:%M:%S'), code, (end_time-start_time).seconds, len(exchange_rate)))
         
-        return stock_price
+        return exchange_rate
         
     def run(self):
         """
@@ -115,11 +115,11 @@ class StockPriceCrawler(DBCrawler):
             Example
             -------
             conn = sqlite3.connect('external_data.db')
-            spc = StockPriceCrawler(conn)
-            spc.set_code(['005830', '005930', '105560'])
-            spc.run()
+            erc = ExchangeRateCrawler(conn)
+            erc.set_code(['FX_USDKRW', 'FX_JPYKRW', 'FX_CNYKRW'])
+            erc.run()
         """
         
         for code in self.codes:
-            stock_price = self.get_stock_price(code)
-            stock_price.to_sql(name=self.table_name, con=self.conn, if_exists='replace', index=False)
+            exchange_rate = self.get_exchange_rate(code)
+            exchange_rate.to_sql(name=self.table_name, con=self.conn, if_exists='replace', index=False)
